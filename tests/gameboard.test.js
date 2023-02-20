b60fa6686eba8261bd4ab1d425ca34df227c8963
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-02-20 19:36:37 +0530

    Updated tests

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -9,26 +9,17 @@ test('Gameboard can place ships', () => {
     const isHorizontal = true;
     expect(gameboard.placeShip(ship, x, y, isHorizontal)).toBe(true);
     expect(gameboard.shipsAt.length).toBe(1);
-    expect(gameboard.shipsAt[0]).toEqual(ship);
+    expect(gameboard.placeShip(ship, x + 3, y + 3, false)).toBe(true);
+    expect(gameboard.shipsAt.length).toBe(2);
 });
 
 test('Gameboard can avoid overlapping the ships', () => {
     const gameboard = gb();
-    const ship1 = Ship(1);
-    const ship2 = Ship(1);
+    const ship = Ship(1);
     const x = 0;
     const y = 0;
     const isHorizontal = true;
-    gameboard.placeShip(ship1, x, y, isHorizontal);
-    expect(gameboard.placeShip(ship2, x, y, isHorizontal)).toBe(false);
-});
-
-test('Gameboard can avoid placing ships out of bounds', () => {
-    const gameboard = gb();
-    const ship = Ship(1);
-    const x = 10;
-    const y = 9;
-    const isHorizontal = true;
+    gameboard.placeShip(ship, x, y, isHorizontal);
     expect(gameboard.placeShip(ship, x, y, isHorizontal)).toBe(false);
 });
 
@@ -39,7 +30,7 @@ test('Gameboard can initialize with 10 random ships', () => {
     expect(gameboard.shipsAt.every(ship => ship.length >= 1 && ship.length <= 4)).toBe(true);
 });
 
-test('Gameboard can receive attacks/ all ships have sunk', () => {
+test('Gameboard can receive attacks', () => {
     const gameboard = gb();
     const ship = Ship(1);
     const x = 0;
@@ -48,6 +39,18 @@ test('Gameboard can receive attacks/ all ships have sunk', () => {
     gameboard.placeShip(ship, x, y, isHorizontal);
     gameboard.receiveAttack(x, y);
     expect(gameboard.getShipAt(x, y).isSunk()).toBe(true);
+});
+
+test('Gameboard can check if all ships have been sunk', () => {
+    const gameboard = gb();
+    const ship = Ship(1);
+    const x = 0;
+    const y = 0;
+    const isHorizontal = true;
+    gameboard.placeShip(ship, x, y, isHorizontal);
+    expect(gameboard.allShipsSunk()).toBe(false);
+    gameboard.receiveAttack(x, y);
+    expect(gameboard.allShipsSunk()).toBe(true);
 });
 
 test('Gameboard can keep track of missed shots', () => {
@@ -75,9 +78,23 @@ test('Gameboard can check if a ship can be placed at a given coordinate', () => 
     const x = 3;
     const y = 3;
     const isHorizontal = false;
-    expect(gameboard.canPlaceShip(ship, x, y, isHorizontal)).toBe(true);
+    expect(gameboard.canPlaceShip(x, y)).toBe(true);
     gameboard.placeShip(ship, x, y, isHorizontal);
-    expect(gameboard.canPlaceShip(ship, x, y, isHorizontal)).toBe(false);
-    expect(gameboard.canPlaceShip(ship, x, y + 1, isHorizontal)).toBe(false);
-    expect(gameboard.canPlaceShip(ship, x, y + 2, isHorizontal)).toBe(false);
+    expect(gameboard.canPlaceShip(x, y)).toBe(false);
+    expect(gameboard.canPlaceShip(x, y + 1)).toBe(false);
+    expect(gameboard.canPlaceShip(x, y + 2)).toBe(false);
+});
+
+test('Gameboard can retrive the ship using the given coordinates', () => {
+    const gameboard = gb();
+    const ship = Ship(3);
+    const x = 3;
+    const y = 3;
+    const isHorizontal = false;
+    gameboard.placeShip(ship, x, y, isHorizontal);
+    // expect(gameboard.getShipAt(x, y)).toBe
+    expect(gameboard.getShipAt(x, y)).toEqual(ship);
+    expect(gameboard.getShipAt(x, y + 1)).toEqual(ship);
+    expect(gameboard.getShipAt(x, y + 2)).toEqual(ship);
+    expect(gameboard.getShipAt(x, y + 3)).toEqual(null);
 });

fb8139f0d1069f328e10d6185ac6357ce63d91dc
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-02-21 20:12:32 +0530

    Added a separate file that lists all available ships

---
+++ js/availableShips.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview This file contains the available ships for the game.
+ */
+import Ship from "./ship.js";
+
+const availableShips = [
+    Ship(4),
+    Ship(3),
+    Ship(3),
+    Ship(2),
+    Ship(2),
+    Ship(2),
+    Ship(1),
+    Ship(1),
+    Ship(1),
+    Ship(1),
+];
+
+export default availableShips;

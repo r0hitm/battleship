fc672b7e14bad2af0ee08a2f6934c47537219533
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-02-28 18:37:09 +0530

    Added webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+    entry: "./js/App.js",
+    output: {
+        filename: "App.js",
+        path: path.resolve(__dirname, "public"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./html/index.html",
+            scriptLoading: "defer",
+        }),
+
+        new MiniCssExtractPlugin(),
+
+        new CssMinimizerPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                ],
+            },
+            {
+                test: /\.html$/i,
+                use: "html-loader",
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "img/[name]-[hash][ext]",
+                },
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+            // `...`,
+            new CssMinimizerPlugin(),
+        ],
+    },
+};
